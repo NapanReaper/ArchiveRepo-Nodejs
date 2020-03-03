6a3d862621bf724998ffaef114da8c2516426a07
DuyDTNSE62826 <duydtnse62826@fpt.edu.vn>
2020-03-03 12:15:00 +0700

    [DuyDTN] save repo.zip to directory

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const child = require("child_process")
+const fs = require('fs')
 const cloneArgs = [
     "clone",
     "https://github.com/DuyDTN/TestLibGit2Sharp.git",
     "--progress"
 ];
 child.spawnSync("git", cloneArgs);
-child.exec('"C:/Program Files/Git/bin/git.exe" archive --format zip --output ../latest.zip master', { cwd: './TestLibGit2Sharp' }, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }    
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-});
+archiveRepo = () => {
+    var dirPath = "tempDir"
+    try {
+        //check if dirPath is exists.
+        fs.statSync(dirPath);
+    } catch (e) {
+        fs.mkdirSync(dirPath);
+    }
+    child.exec('"C:/Program Files/Git/bin/git.exe" archive --format zip --output ../tempDir/latest.zip master', { cwd: './TestLibGit2Sharp' }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+archiveRepo();
