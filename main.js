bbfce3960bb260a1e2b94fe39c16b923a50070b4
DuyDTNSE62826 <duydtnse62826@fpt.edu.vn>
2020-03-04 10:49:40 +0700

    [DuyDTN] add archive from commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const child = require("child_process")
 const fs = require('fs')
 const cloneArgs = [
     "clone",
-    "https://github.com/DuyDTN/TestLibGit2Sharp.git",
+    "https://github.com/NapanReaper/TestGitArchive.git",
     "--progress"
 ];
 child.spawnSync("git", cloneArgs);
@@ -14,7 +14,7 @@ archiveRepo = () => {
     } catch (e) {
         fs.mkdirSync(dirPath);
     }
-    child.exec('"C:/Program Files/Git/bin/git.exe" archive --format zip --output ../tempDir/latest.zip master', { cwd: './TestLibGit2Sharp' }, (error, stdout, stderr) => {
+    child.exec('"C:/Program Files/Git/bin/git.exe" archive --format zip --output ../tempDir/latest.zip 18b7cf6', { cwd: './TestGitArchive' }, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
